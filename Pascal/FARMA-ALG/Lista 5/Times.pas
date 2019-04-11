66b95e83516e4fe6c59044f2de10a2a741dd7470
Gabriel Henrique <gabriel.henriqueomello@gmail.com>
2019-04-11 18:29:01 -0300

    times otimizado

--- Pascal/FARMA-ALG/Lista 5/Times.pas
+++ Pascal/FARMA-ALG/Lista 5/Times.pas
@@ -3,21 +3,18 @@ program Times;
 var
   times: array [1..20] of string;
   pontuacao: array [1..20] of longint;
-  maior: array [1..2] of longint;
-  i, max: integer;
+  i, max, maior: integer;
 begin
   readln(max);
   for i := 1 to max do
     readln(times[i]);
   for i := 1 to max do
     readln(pontuacao[i]);
-  maior[1] := pontuacao[1];
-  maior[2] := 1;
+  maior := 1;
   for i := 2 to max do
-    if pontuacao[i] > maior[1] then
+    if pontuacao[i] > pontuacao[maior] then
     begin
-      maior[1] := pontuacao[i];
-      maior[2] := i;
+      maior := i;
     end;
-  writeln('O campeao eh o ', times[maior[2]]);
+  writeln('O campeao eh o ', times[maior]);
 end.
