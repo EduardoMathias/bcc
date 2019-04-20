2fb8eba20e0895c626112bfc86bdb1cb3c4c4f94
gabrielhom <gabriel.henriqueomello@gmail.com>
2019-04-20 01:23:19 -0300

    Aniversario.pas refactoring

--- Pascal/FARMA-ALG/Lista 6/Aniversario.pas
+++ Pascal/FARMA-ALG/Lista 6/Aniversario.pas
@@ -1,4 +1,4 @@
-{ WTF 100 VETORES PRA QUE??? QUER FORÇAR A USAR MATRIZ???????????? }
+{ WTF 100 VETORES PRA QUE??? QUER FORÇAR A USAR aniversarios???????????? }
 
 program Aniversario;
 
@@ -6,32 +6,31 @@ const MAX = 100;
 
 var 
   i, j, k, n, x: Integer;
-  matriz : array [1..MAX, 1..30] of Integer;
+  aniversarios : array [1..30] of Integer;
   found : Boolean;
   soma : Real;
 begin
   read(n);
-  for i := 1 to MAX do
-    for j := 1 to n do
-      begin
-        read(x);
-        matriz[i][j] := x;
-      end;
   soma := 0.0;
   for i := 1 to MAX do
     begin
+      for j := 1 to n do
+        begin
+          read(x);
+          aniversarios[j] := x;
+        end;
       j := 1;
       found := False;
       while (not found) and (j < n) do
         begin
           for k := 1 to n do
             if k <> j then
-              if matriz[i][k] = matriz[i][j] then
+              if aniversarios[k] = aniversarios[j] then
                 found := True;
           j := j + 1;
+          if found then
+            soma := soma + 1;
         end;
-      if found then
-        soma := soma + 1;
     end;
   WriteLn(soma / MAX:0:2);
 end.
